eddff4efaf6077bb8df0f6ab2b5b11337ed829b7
chris imoni <imonireal@gmail.com>
2023-07-14 15:45:14 +0100

    added similated delay

--- src/main/java/com/chrisimoni/reactiverestapi/service/StudentService.java
+++ src/main/java/com/chrisimoni/reactiverestapi/service/StudentService.java
@@ -7,13 +7,16 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+
 @Service
 @RequiredArgsConstructor
 public class StudentService {
     private final StudentRepository repository;
 
     public Flux<Student> findAll() {
-        return repository.findAll();
+        return repository.findAll()
+                .delayElements(Duration.ofSeconds(1));
     }
 
     public Mono<Student> findById(Integer id) {
